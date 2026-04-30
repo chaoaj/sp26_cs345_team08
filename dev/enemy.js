cd0a3a3f98a8773d7d8860b7930a0fbf35062ee0
Alex Grant <grantai@dukes.jmu.edu>
2026-04-29 20:45:47 -0400

    Removed old healthbar system

--- dev/enemy.js
+++ dev/enemy.js
@@ -177,23 +177,5 @@ class Enemy {
       );
       return;
     }
-
-    let squareSize = 7;
-    let gap = 2;
-    let totalWidth = 4 * squareSize + 3 * gap;
-    let healthX = this.pos.x - totalWidth / 2;
-    let healthY = this.pos.y - 30;
-
-    for (let i = 0; i < 4; i++) {
-      let x = healthX + i * (squareSize + gap);
-      if (i < this.health) {
-        fill(0, 255, 0);
-      } else {
-        fill(80);
-      }
-      stroke(0);
-      strokeWeight(1);
-      rect(x, healthY, squareSize, squareSize);
-    }
   }
 }
